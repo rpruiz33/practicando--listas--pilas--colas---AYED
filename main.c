bfedae4f0052fef68b8a120df1e0fdf4e8e4c68d
unknown <rpruiz33@gmail.com>
2024-01-18 12:55:31 -0300

    avl

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ typedef struct ListaE * ListaPtr;
 typedef struct PersonaE * PersonaPtr;
 typedef struct ColaE * ColaPtr;
 typedef struct PilaE * PilaPtr;
-typedef struct NodaA * NodoAptr;
+typedef struct NodoA * NodoAptr;
 ///ahora trabajamos con arboles avl
 struct NodoA{
 int fe;
@@ -360,6 +360,43 @@ void mostrarPila(PilaPtr l){
     }
 }
 
+void rotacionSImpleIzquierda(NodoAptr n, NodoAptr n1){
+
+if (n->fe==2 || n1->fe==0){
+    n->der=n1->izq;
+    n1->izq=n;
+    n=n1;
+}
+}
+void rotacionSImpleDerecha(NodoAptr n,NodoAptr n1){
+if((n->fe=-2 )|| (n1->fe<=0)){
+n->izq=n1->der;
+n1->der=n;
+n=n1;
+
+}
+
+}
+void rotoacionDobleIzquirdaDerecha(NodoAptr n1,NodoAptr n2 , NodoAptr n ){
+    if((n->fe=-2 )|| (n1->fe>0)){
+n1->der=n2->izq;
+n2->izq=n1;
+n->izq=n2->der;
+n2->der=n;
+n=n2;
+
+}
+}
+void rotoacionDobleDerechaIzquierda(NodoAptr n1,NodoAptr n2 , NodoAptr n ){
+    if((n->fe=-2 )|| (n1->fe<0)){
+n1->izq=n2->der;
+n2->der=n1;
+n->der=n2->izq;
+n2->izq=n;
+n=n2;
+
+}
+}
 int main()
 {
 PersonaPtr p1=crearPersona(33,"rober");
